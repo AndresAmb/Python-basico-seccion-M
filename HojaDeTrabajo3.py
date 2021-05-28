b46e8c5615abd51dd274ce77631e6e35fac8aa3f
AndresAmb <84362835+AndresAmb@users.noreply.github.com>
2021-05-28 15:56:07 -0600

    Add files via upload

---
+++ HojaDeTrabajo3.py
@@ -0,0 +1,30 @@
+print("--------------------------------------------------------------------------------")
+contraseña=input("Ingrese una nueva contraseña: ").upper()
+vcontraseña=input("Verifique su contraseña: ").upper()
+while contraseña!=vcontraseña:
+    vcontraseña=input("Las contraseñas no coinciden, vuelva a intentarlo: ").upper()
+print("Contraseña verificada")
+print("--------------------------------------------------------------------------------")
+nEntrad=input("Nueva entrada (Si/No): ").upper()
+while nEntrad=="SI":
+    nombre=input("Ingrese su nombre: ").upper()
+    sexo=input("Ingrse su sexo (hombre/mujer): ").upper()
+    pletra=nombre[0]
+    while sexo!="MUJER" and sexo!="HOMBRE":
+            sexo=input("El genero ingresado no es valido, intente de nuevo: ").upper()
+    if sexo=="MUJER":
+        if pletra<"M":
+            print("Usted pertenece al grupo A")
+        else:
+            print("Ustes pertenece al grupo B")
+    elif sexo=="HOMBRE":
+        if pletra>"N":
+            print("Usted pertenece al grupo A")
+        else:
+            print("Usted pertenece al grupo B")
+    else:
+        print()
+    print()
+    nEntrad=input("Nueva entrada (Si/No): ").upper()
+print("Que tenga un feliz día")
+print("--------------------------------------------------------------------------------")
